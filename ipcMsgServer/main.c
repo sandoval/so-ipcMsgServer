283d85ea1a1c227f7315ccf25be03eac1e3443b4
Daniel Sandoval <daniel@loopec.com.br>
2013-11-06 09:44:23 -0200

    Implemented multiple threads to handle different type of messages.

--- ipcMsgServer/main.c
+++ ipcMsgServer/main.c
@@ -13,6 +13,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <pthread.h>
 
 #define POOL_SIZE 5
 typedef struct intList intList;
@@ -37,6 +38,7 @@ void removeValue(int value, intList **list);
 intList *putIntoIntList(int value, intList *list);
 
 struct connectedClients connectedClients;
+pthread_mutex_t clientConnectionMutex = PTHREAD_MUTEX_INITIALIZER;
 
 void treatSigint() {
     printf("Received SIGINT!\n");
@@ -44,51 +46,90 @@ void treatSigint() {
     exit(1);
 }
 
-void receiveMessage(message* msg) {
-    printf("[SERVER] Message %ld received from %d\n", msg->mdata.messageId, msg->mdata.source);
+void entryMessageHandler(message* msg) {
+    printf("[SERVER] Handling entryMessage\n");
     message response;
     response.mdata.destination = msg->mdata.source;
     response.mdata.referencedMessageId = msg->mdata.messageId;
-    if (msg->mtype == 1) {
-        response.mtype = 1;
-        if (connectClient(msg->mdata.source)) {
-            response.mdata.text[0] = 'y';
-        } else {
-            response.mdata.text[0] = 'n';
-        }
-        printf("[SERVER] Client %d connected: %c\n", msg->mdata.source, response.mdata.text[0]);
-    } else if (msg->mtype == 2) {
-        response.mtype = 2;
-        if (disconnectClient(msg->mdata.source)) {
-            response.mdata.text[0] = 'y';
-        } else {
-            response.mdata.text[0] = 'n';
-        }
-        printf("[SERVER] Client %d disconnected: %c\n", msg->mdata.source, response.mdata.text[0]);
-    } else if (msg->mtype == 3) {
-        response.mtype = 3;
-        if (clientIsConnected(msg->mdata.source)) {
-            printf("%s", msg->mdata.text);
-            response.mdata.text[0] = 'y';
-        } else {
-            response.mdata.text[0] = 'n';
-        }
+    response.mtype = 1;
+    if (connectClient(msg->mdata.source)) {
+        response.mdata.text[0] = 'y';
     } else {
-        return;
+        response.mdata.text[0] = 'n';
     }
+    printf("[SERVER] entryMessage check\n");
     sendMessage(&response);
+    printf("[SERVER] Client %d connected: %c\n", msg->mdata.source, response.mdata.text[0]);
+}
+
+void *entryManager(void *threadid) {
+    listenForMessages(1, entryMessageHandler);
+    return NULL;
+}
+
+void exitMessageHandler(message* msg) {
+    printf("[SERVER] Handling exitMessage\n");
+    message response;
+    response.mdata.destination = msg->mdata.source;
+    response.mdata.referencedMessageId = msg->mdata.messageId;
+    response.mtype = 2;
+    if (disconnectClient(msg->mdata.source)) {
+        response.mdata.text[0] = 'y';
+    } else {
+        response.mdata.text[0] = 'n';
+    }
+    printf("[SERVER] exitMessage check\n");
+    sendMessage(&response);
+    printf("[SERVER] Client %d disconnected: %c\n", msg->mdata.source, response.mdata.text[0]);
+}
+
+void *exitManager(void *threadid) {
+    listenForMessages(2, exitMessageHandler);
+    return NULL;
+}
+
+void printMessageHandler(message* msg) {
+    printf("[SERVER] Handling printMessage\n");
+    message response;
+    response.mdata.destination = msg->mdata.source;
+    response.mdata.referencedMessageId = msg->mdata.messageId;
+    response.mtype = 3;
+    if (clientIsConnected(msg->mdata.source)) {
+        printf("%s", msg->mdata.text);
+        response.mdata.text[0] = 'y';
+    } else {
+        response.mdata.text[0] = 'n';
+    }
+    printf("[SERVER] printMessage check\n");
+    sendMessage(&response);
+}
+
+void *printManager(void *threadid) {
+    listenForMessages(3, printMessageHandler);
+    return NULL;
 }
 
 int main(int argc, const char * argv[])
 {
     struct sigaction action;
+    pthread_t threads[2];
 
     action.sa_handler = treatSigint;
     sigaction(SIGINT, &action, NULL);
     
     setup(INT_MAX);
     
-    listenForMessages(0, receiveMessage);
+    if (pthread_create(&threads[0], NULL, entryManager, NULL)) {
+        printf("ERROR creating entryManager thread.\n");
+        exit(1);
+    }
+    if (pthread_create(&threads[1], NULL, exitManager, NULL)) {
+        printf("ERROR creating exitManager thread.\n");
+        exit(1);
+    }
+    
+    //Start print manager on main thread.
+    (void)printManager(NULL);
     
     tearDown();
     
@@ -96,25 +137,38 @@ int main(int argc, const char * argv[])
 }
 
 int clientIsConnected(int client) {
-    return hasValue(client, connectedClients.clients);
+    int ret;
+    pthread_mutex_lock(&clientConnectionMutex);
+    ret = hasValue(client, connectedClients.clients);
+    pthread_mutex_unlock(&clientConnectionMutex);
+    return ret;
 }
 
 int connectClient(int client) {
-    if (hasValue(client, connectedClients.clients))
+    pthread_mutex_lock(&clientConnectionMutex);
+    if (hasValue(client, connectedClients.clients)) {
+        pthread_mutex_unlock(&clientConnectionMutex);
         return 1;
-    if (connectedClients.number >= POOL_SIZE)
+    }
+    if (connectedClients.number >= POOL_SIZE) {
+        pthread_mutex_unlock(&clientConnectionMutex);
         return 0;
+    }
     connectedClients.number++;
     connectedClients.clients = putIntoIntList(client, connectedClients.clients);
+    pthread_mutex_unlock(&clientConnectionMutex);
     return 1;
 }
 
 int disconnectClient(int client) {
+    pthread_mutex_lock(&clientConnectionMutex);
     if (hasValue(client, connectedClients.clients)) {
         connectedClients.number--;
         removeValue(client, &(connectedClients.clients));
+        pthread_mutex_unlock(&clientConnectionMutex);
         return 1;
     }
+    pthread_mutex_unlock(&clientConnectionMutex);
     return 0;
 }
 
